c2ed7bd63bf92e0f9ff23217cb40b2a6197b0570
Lauro Molina <molina.lauro@gmail.com>
2013-07-11 16:47:05 +0000

    updated web.js again and again

--- web.js
+++ web.js
@@ -1,13 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var content = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-var fs = require('fs');
-var content = fs.readFileSync('index.html','utf-8');
-responose.send(content);
+  response.send(content.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-console.log("Listening on " + port);
+  console.log("Listening on " + port);
 });
