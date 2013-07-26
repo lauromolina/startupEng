5a810e46f4faf692d26559d179a38981ca53abfe
lauromolina <molina.lauro@gmail.com>
2013-07-26 12:35:08 +0000

    working on port 8080

--- web.js
+++ web.js
@@ -4,14 +4,12 @@ var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
-
-
 app.get('/', function(request, response) {
-  var content = fs.readFileSync('index.html');
-  response.send(html);
+   var html = fs.readFileSync(htmlfile).toString();
+   response.send(html);
 });
 
 var port = process.env.PORT || 8080;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(port, function(){
+ console.log("Listening on " + port);
+}); 
