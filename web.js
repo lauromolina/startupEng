89ef4ad16f4dfe44814f56e08c63535cf474dff4
Ubuntu <ubuntu@ip-172-31-31-215.us-west-2.compute.internal>
2013-07-26 10:12:30 +0000

    as in lecture 7

--- web.js
+++ web.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html";
+
 var app = express.createServer(express.logger());
 
-var content = fs.readFileSync('index.html');
+
 
 app.get('/', function(request, response) {
-  response.send(content.toString("utf-8"));
+  var content = fs.readFileSync('index.html');
+  response.send(html));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
